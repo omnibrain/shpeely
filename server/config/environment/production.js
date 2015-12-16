4bfa58a3c77ab6bf8334a036b61babfe31f6fb3d
Raphael <r.voellmy@gmail.com>
2015-12-16 18:19:29 +0100

    seed in production as well

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -24,5 +24,7 @@ module.exports = {
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/boardgametournament'
-  }
+  },
+
+  seedDB: true
 };
