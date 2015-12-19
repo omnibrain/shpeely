d8adbb48e97b4e142c7d2584dd36886937aee6b0
Raphael <r.voellmy@gmail.com>
2015-12-19 20:48:17 +0100

    server api call to disconnect user from player

--- server/api/player/index.js
+++ server/api/player/index.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var controller = require('./player.controller');
+var auth = require('../../auth/auth.service.js');
 
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
+router.post('/disconnect', auth.isAuthenticated(), controller.disconnect);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
