212d45ac65bab353367f09e57ad94f843b0e71ea
Raphael <r.voellmy@gmail.com>
2015-12-20 15:37:04 +0100

    restrict tournament name to 50 characters

--- server/api/tournament/tournament.model.js
+++ server/api/tournament/tournament.model.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var slug = require('mongoose-url-slugs');
 
 var TournamentSchema = new Schema({
-  name: {type: String, required: true},
+  name: {type: String, required: true, maxlength: 50},
   active: {type: Boolean, default: true, required: true},
   created: {type: Date, default: Date.now, required: true},
   members: [ {type : mongoose.Schema.ObjectId, ref : 'Player'} ],
